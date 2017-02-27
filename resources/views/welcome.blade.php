e46ad03be30972ca07d2287c07856c0b6e7c8e4d
putraranji <ogidoank51@yahoo.com>
2017-02-27 11:54:48 +0700

    Modified main page and add link contact to it

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -46,11 +46,11 @@
             }
 
             .title {
-                font-size: 84px;
+                font-size: 50px;
             }
 
             .links > a {
-                color: #636b6f;
+                color: blue;
                 padding: 0 25px;
                 font-size: 12px;
                 font-weight: 600;
@@ -79,15 +79,11 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Basic CRUD Contact Manager
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="{{ url('/contact') }}">Sample List of Contact</a>
                 </div>
             </div>
         </div>
